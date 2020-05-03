45e202113dbc65d3724dd4a5a4cea07cdb1af0a8
Felipe Peña <felipe@penya.cl>
2020-05-02 19:40:28 -0700

    Add conference and newsletter component

---
+++ resources/views/components/conference.blade.php
@@ -0,0 +1,78 @@
+<div>
+    <div class="bg-white">
+        <div class="container mx-auto py-12 text-white flex flex-row px-8 md:px-0">
+            <div class="w-ful px-2">
+                <span class="text-n5_pink font-bold leading-none">Comunidad</span>
+                <h2 class="font-bold text-4xl text-n5_purple mb-4">Conferencia</h2>
+                <p class="text-xl text-gray-800 mb-4">
+                    Desde 2016, reunimos a una comunidad en la principal conferencia anual sobre trabajo remoto y tecnología de Latinoamérica. Nos han acompañado más de 100 speakers y compañías referentes a nivel internacional compartiendo sus conocimientos y experiencias.
+                </p>
+
+                <p class="text-xl text-gray-800 mb-12">
+                    Ten una vista general a la última versión.
+                </p>
+
+                <div class="embed-container">
+                    <iframe class="embed-iframe" src="https://player.vimeo.com/video/390838158" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                </div>
+
+                <div class="flex flex-col md:flex-row mt-12">
+
+                    <div class="w-full md:w-1/2 text-left md:text-center mb-8 md:mb-0">
+                        <button class="button-indigo" onclick="window.location.href='https://2019.9punto5.cl'">
+                            Más sobre la conferencia
+                        </button>
+                    </div>
+                    <div class="w-full md:w-1/2 text-left md:text-center">
+                        <button class="button-pink" onclick="window.location.href='https://vimeo.com/9punto5/videos'">
+                            Ver charlas anteriores
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white">
+        <div class="container mx-auto py-12 text-white px-8 md:px-0">
+            <h4 class="font-bold text-2xl mb-4 text-n5_purple">¿Te gustaría ser sponsor de 9punto5?</h4>
+
+            <p class="text-xl leading-normal text-black mb-4">
+                Desde el 2016, hemos tenido múltiples empresas apoyando nuestra misión a través de auspicios.<br>
+                Sin ellas, sería imposible la realización de esta conferencia.<br><br>
+                9punto se nutre constantemente con estos apoyos y ha creado una comunidad de más de 3.000 personas apoyando el trabajo remoto.
+            </p>
+
+            <h4 class="text-n5_pink text-xl font-bold">Algunas marcas que han confiado en 9punto5</h4>
+
+            <ul class="mt-4 mb-6 flex flex-col md:flex-row items-center">
+                <li class="mr-0 md:mr-8">
+                    <img :src="{{ asset('img/evernote.png') }}" alt="Evernote" width="160">
+                </li>
+                <li class="mt-8 md:mt-0 mr-0 md:mr-8">
+                    <img :src="{{ asset('img/doist.png') }}" alt="Doist" width="130">
+                </li>
+                <li class="mt-8 md:mt-0 mr-0 md:mr-8">
+                    <img :src="{{ asset('img/google.png') }}" alt="Google Developers" width="160">
+                </li>
+                <li class="mt-8 md:mt-0 mr-0 md:mr-8">
+                    <img :src="{{ asset('img/walmart.png') }}" alt="Walmart Chile" width="160">
+                </li>
+                <li class="mt-8 md:mt-0 mr-0 md:mr-8">
+                    <img :src="{{ asset('img/cornershop.png') }}" alt="Cornershop" width="160">
+                </li>
+                <li class="mt-8 md:mt-0">
+                    <img :src="{{ asset('img/compara.png') }}" alt="Compara Online" width="160">
+                </li>
+            </ul>
+
+            <p class="text-xl leading-normal text-black mb-4">
+                ¿Te gustaría ser un sponsor? Envíanos un mensaje para mostrarte lo que podemos lograr juntos.
+            </p>
+
+            <div class="w-full md:w-2/5">
+
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,11 +1,13 @@
 <div class="bg-white py-20">
-    <div class="container mx-auto bg-n5_purple_darker text-white px-10 py-16 rounded-lg">
+    <div class="container mx-auto bg-n5_purple_darker text-white px-10 py-16 rounded-lg flex flex-row">
         <div class="w-1/4">
             <span class="text-sm font-bold leading-none uppercase">Mantente informado</span>
             <h3 class="text-4xl">Newsletter</h3>
         </div>
         <div class="w-3/4">
-            
+            <div class="_form_30"></div>
+            <script src="https://9punto5.activehosted.com/f/embed.php?id=30" type="text/javascript"
+                    charset="utf-8"></script>
         </div>
     </div>
 </div>
